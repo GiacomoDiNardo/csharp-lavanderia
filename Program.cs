136b404cac02258e7db9e3a7c6f619e8c7dc89a3
GiacomoDiNardo <vest_95@hotmail.it>
2022-11-03 17:43:52 +0100

    aggiunto uso randomico dei macchinari e stampa dettagli macchinario scelto

--- Program.cs
+++ Program.cs
@@ -1,25 +1,140 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-//5 Lavatrici e 5 Asciugatrici.
-
 using System.Security.Cryptography.X509Certificates;
+//instazio le lavatrici e asciugatrici
+Lavatrice[] lavatrici = new Lavatrice[5];
+Asciugatrice[] asciugatrici = new Asciugatrice[5];
 
-string[] lavatrici = { "lavatrice1", "lavatrice2", "lavatrice3", "lavatrice4", "lavatrice5" };
-string[] asciugatrici = { "asciugatrice1", "asciugatrice2", "asciugatrice3", "asciugatrice4", "asciugatrice5" };
+for (int i = 0; i < lavatrici.Length; i++)
+{
+    lavatrici[i] = new Lavatrice();
+}
 
-Lavatrice lavatrice1 = new Lavatrice();
-Lavatrice lavatrice2 = new Lavatrice();
-lavatrice1.Rinnovante();
-lavatrice2.Sgrassante();
-lavatrice1.Rinfrescante();
+for (int i = 0; i < asciugatrici.Length; i++)
+{
+    asciugatrici[i] = new Asciugatrice();
+}
+
+//creo un numero casuale di lavaggi
+int lavaggi = new Random().Next(1, 100);
+
+
+//assegno dei lavaggi casuali a delle lavatrici casuali
+for (int i = 0; i < lavaggi; i++)
+{
+    //seleziono casualmente una lavatrice
+    int lavatriceRandom = new Random().Next(0, 5);
+    //seleziono casualmente un programma
+    int random = new Random().Next(0, 4);
+
+    if ( random == 1 )
+    {
+        lavatrici[lavatriceRandom].Rinfrescante();
+    } else if (random == 2 )
+    {
+        lavatrici[lavatriceRandom].Rinnovante();
+    } else if ( random == 3 )
+    {
+        lavatrici[lavatriceRandom].Sgrassante();
+    } else
+    {
+        lavatrici[lavatriceRandom].InUso = false;
+        lavatrici[lavatriceRandom].Tempo = 0;
+    }
+}
+
+//verifico quali lavatrici sono attualmente in uso
+for ( int i = 0; i < lavatrici.Length; i++)
+{
+    if (lavatrici[i].InUso == true)
+    {
+        Console.WriteLine("La lavatrice " + i + " è in funzione");
+    }
+    else
+    {
+        Console.WriteLine("La lavatrice " + i + " è ferma");
+    }
+}
+
+Console.WriteLine();
+
+//assegno dei lavaggi casuali a delle asciugatrici casuali
+for (int i = 0; i < lavaggi; i++)
+{
+    //seleziono casualmente una lavatrice
+    int asciugatriceRandom = new Random().Next(0, 5);
+    //seleziono casualmente un programma
+    int random = new Random().Next(0, 3);
+
+    if (random == 1)
+    {
+        asciugatrici[asciugatriceRandom].Intenso();
+    }
+    else if (random == 2)
+    {
+        asciugatrici[asciugatriceRandom].Rapido();
+    }
+    else
+    {
+        asciugatrici[asciugatriceRandom].InUso = false;
+        asciugatrici[asciugatriceRandom].Tempo = 0;
+    }
+}
+
+for (int i = 0; i < asciugatrici.Length; i++)
+{
+    if (asciugatrici[i].InUso == true)
+    {
+        Console.WriteLine("La asciugatrice " + i + " è in funzione");
+    }
+    else
+    {
+        Console.WriteLine("La asciugatrice " + i + " è ferma");
+    }
+}
+
+Console.WriteLine();
+
+//visualizzo i dettagli di una lavatrice
+Console.WriteLine("scegli una lavatrice (da 0 a 4)");
+int lavatriceScelta = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(lavatrici[lavatriceScelta].Detersivo + " ml rimasti");
+Console.WriteLine(lavatrici[lavatriceScelta].Ammorbidente + " ml rimasti");
+Console.WriteLine(lavatrici[lavatriceScelta].Gettoni + " gettoni");
+Console.WriteLine(lavatrici[lavatriceScelta].inUso);
+Console.WriteLine(lavatrici[lavatriceScelta].Tempo + " min");
+
+Console.WriteLine();
+
+//visualizzo i dettagli di una asciugatrice
+Console.WriteLine("scegli un'asciugatrice (da 0 a 4)");
+int asciugatriceScelta = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(asciugatrici[asciugatriceScelta].Gettoni + " gettoni");
+Console.WriteLine(asciugatrici[asciugatriceScelta].inUso);
+Console.WriteLine(asciugatrici[asciugatriceScelta].Tempo + " min");
+
+Console.WriteLine();
+
+//calcolo l'incasso del giorno
+double incasso = 0;
+
+for (int i = 0; i < lavatrici.Length; i++)
+{
+    incasso = incasso + (lavatrici[i].Gettoni + asciugatrici[i].Gettoni);
+}
+
+Console.WriteLine("L'incasso è: " + incasso * 0.5 + " euro");
+
 public class Lavatrice
 {
-    private int ammorbidente = 1000;
-    private int detersivo = 500;
-    private bool inUso;
+    private int ammorbidente = 500;
+    private int detersivo = 1000;
+    public bool inUso = false;
     private int gettoni;
-    private int tempo;
+    private int tempo = 0;
 
     public int Ammorbidente
     {
@@ -54,6 +169,7 @@ public class Lavatrice
 
         set
         {
+
             inUso = value;
         }
     }
@@ -79,8 +195,12 @@ public class Lavatrice
 
         set
         {
-            int tPass = new Random().Next(0, value);
-            tempo = value - tPass;
+            tempo = 0;
+            if (InUso == true)
+            {
+                int tPass = new Random().Next(0, value);
+                tempo = value - tPass;
+            }
         }
     }
 
@@ -116,6 +236,7 @@ public class Asciugatrice
 {
     private int gettoni;
     private int tempo;
+    public bool inUso;
 
     public int Gettoni
     {
@@ -144,15 +265,30 @@ public class Asciugatrice
         }
     }
 
+    public bool InUso
+    {
+        get
+        {
+            return inUso;
+        }
+
+        set
+        {
+            inUso = value;
+        }
+    }
+
     public void Rapido ()
     {
         this.Gettoni = 2;
         this.Tempo = 30;
+        this.InUso = true;
     }
 
     public void Intenso()
     {
         this.Gettoni = 4;
         this.Tempo = 60;
+        this.InUso = true;
     }
 }
